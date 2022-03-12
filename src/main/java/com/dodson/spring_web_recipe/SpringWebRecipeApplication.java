055596abc8b038b58a365df99a41ef52ca349756
Lee <hldodsoniii@gmail.com>
2022-03-12 13:18:12 +0000

    initial commit

---
+++ src/main/java/com/dodson/spring_web_recipe/SpringWebRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.dodson.spring_web_recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dodson/spring_web_recipe/SpringWebRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodson.spring_web_recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
