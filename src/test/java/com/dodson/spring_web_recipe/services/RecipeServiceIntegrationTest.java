32987a38a3192b961c98a409f0c3c011449bed69
Lee <hldodsoniii@gmail.com>
2022-04-17 18:34:59 +0000

    added recipe service integration test

---
+++ src/test/java/com/dodson/spring_web_recipe/services/RecipeServiceIntegrationTest.java
@@ -0,0 +1,52 @@
+package com.dodson.spring_web_recipe.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.transaction.Transactional;
+
+import com.dodson.spring_web_recipe.commands.RecipeCommand;
+import com.dodson.spring_web_recipe.converters.RecipeCommandToRecipe;
+import com.dodson.spring_web_recipe.converters.RecipeToRecipeCommand;
+import com.dodson.spring_web_recipe.domain.Recipe;
+import com.dodson.spring_web_recipe.repositories.RecipeRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RecipeServiceIntegrationTest {
+
+    public static final String NEW_DESCRIPTION = "New Description";
+
+    @Autowired
+    RecipeService recipeService;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Autowired
+    RecipeCommandToRecipe recipeCommandToRecipe;
+
+    @Autowired
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Transactional
+    @Test
+    public void testSaveOfDescription() throws Exception {
+        //given
+        Iterable<Recipe> recipes = recipeRepository.findAll();
+        Recipe testRecipe = recipes.iterator().next();
+        RecipeCommand testRecipeCommand = recipeToRecipeCommand.convert(testRecipe);
+
+        //when
+        testRecipeCommand.setDescription(NEW_DESCRIPTION);
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(testRecipeCommand);
+
+        //then
+        assertEquals(NEW_DESCRIPTION, savedRecipeCommand.getDescription());
+        assertEquals(testRecipe.getId(), savedRecipeCommand.getId());
+        assertEquals(testRecipe.getCategories().size(), savedRecipeCommand.getCategories().size());
+        assertEquals(testRecipe.getIngredients().size(), savedRecipeCommand.getIngredients().size());
+    }
+}
