ec4714d3be0929b6b7a8aff241a36204311d0382
Lee <hldodsoniii@gmail.com>
2022-04-18 16:12:49 +0000

    added form to add a new recipe

--- src/main/java/com/dodson/spring_web_recipe/controllers/RecipeController.java
+++ src/main/java/com/dodson/spring_web_recipe/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.dodson.spring_web_recipe.controllers;
 
+import com.dodson.spring_web_recipe.commands.RecipeCommand;
 import com.dodson.spring_web_recipe.services.RecipeService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,19 @@ public class RecipeController {
     public String showById(Model model, @PathVariable String recipeId) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(recipeId)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:show/" + savedRecipeCommand.getId();
     }
 }
